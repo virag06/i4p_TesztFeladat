a2e49624fbe6f41b2b5f1cfc77c795558ebb5dce
Nyakó Virág <nyvirag06@gmail.com>
2026-05-23 23:10:11 +0200

    Az első feladat vége, létrehoztam a dekódoló függvényt

--- 1.feladat/functions.py
+++ 1.feladat/functions.py
@@ -1,25 +1,34 @@
 import string
 
+abc=string.ascii_lowercase
+
+kodBetu={}
+kodSzam={}
+
+for i in range (0,len(abc)):
+    kodBetu[abc[i]] = i
+    kodSzam[i]=abc[i]
+kodBetu[" "]=26
+kodSzam[26]=" "
+
 def kodolo(uzenet, kulcs):
-
-    abc=string.ascii_lowercase
-
-    kodBetu={}
-    kodSzam={}
     rejtjelezettUz=""
 
-
-    for i in range (0,len(abc)):
-        kodBetu[abc[i]] = i
-        kodSzam[i]=abc[i]
-
-    kodBetu[" "]=26
-    kodSzam[26]=" "
-
-    for i in range (0,len(uzenet)):
+    for i in range(0,len(uzenet)):
         eredmeny=kodBetu[uzenet[i]]+kodBetu[kulcs[i]]
         if eredmeny > 26:
             eredmeny=eredmeny%27
         rejtjelezettUz+=kodSzam[eredmeny]
 
     return rejtjelezettUz
+
+def dekodolo(rejtjelezett,kulcs):
+    eredetiUz=""
+
+    for i in range(0,len(rejtjelezett)):
+        eredeti=kodBetu[rejtjelezett[i]]-kodBetu[kulcs[i]]
+        if eredeti < 0:
+            eredeti=eredeti+27
+        eredetiUz+=kodSzam[eredeti]
+
+    return eredetiUz
--- 1.feladat/main.py
+++ 1.feladat/main.py
@@ -3,6 +3,8 @@ from functions import *
 
 uzenet="helloworld"
 kulcs="abcdefgijkl"
+rejtjelezett="hfnosauzun"
 
 
 print("A rejtjelezett üzenet: ", kodolo(uzenet,kulcs))
+print("Az eredeti üzenet: ", dekodolo(rejtjelezett,kulcs))
